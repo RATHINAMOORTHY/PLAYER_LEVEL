9e95d612d81d6b1e72b3a6de3354d146c470d257
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-11 05:27:11 +0530

    Update reverse_and_remove_vowel.java

--- reverse_and_remove_vowel.java
+++ reverse_and_remove_vowel.java
@@ -1,1 +1,16 @@
-
+class reverse_and_remove_vowel
+{
+  public static void main(String args[])
+  {
+    StringBuffer sb=new StringBuffer(args[0]);
+    System.out.print("REVERSED");
+    System.out.println(sb.reverse());
+    System.out.print("VOWEL REMOVED");
+    char[] ch=args[0].toCharArray();
+    for(int i=0;i<args[0].length();i++)
+    {
+      if(ch[i]!='a'||ch[i]!='e'||ch[i]!='i'||ch[i]!='o'||ch[i]!='u')
+        System.out.print(ch[i]);
+    }
+  }
+}
