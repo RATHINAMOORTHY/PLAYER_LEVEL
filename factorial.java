8e5e3ce442d4175ecf213d01f8b3892f41fa32ab
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 19:18:20 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,1 +1,13 @@
-
+class factorial
+{
+  public static void main(String args[])
+  {
+    int total=1;
+    int a=Integer.valueOf(args[0]);
+    for(int i=a;i>=0;i--)
+    {
+      total=total*i;
+    }
+    System.out.print(+total);
+  }
+}
