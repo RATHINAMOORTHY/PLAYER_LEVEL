fbdb4fde332b822b05ea667b2fbf22e4f61a516a
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 21:18:13 +0530

    Update holiday.java

--- holiday.java
+++ holiday.java
@@ -3,12 +3,11 @@ class holiday
   public static void main(String args[])
   {
         
-           if(args[0].equalsIgnoreCase("sunday");
+           if(args[0].equalsIgnoreCase("sunday"))
               System.out.print("TRUE");
            else
               System.out.print("FALSE");
               }
               }
-       
               
       
