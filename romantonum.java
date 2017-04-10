87174411276dd9f2242c9305937e75627bf44f53
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 20:33:07 +0530

    Update romantonum.java

--- romantonum.java
+++ romantonum.java
@@ -1,7 +1,34 @@
 class romantonum
 {
-  public static void main(String args[])
+   public static void main(String args[])
   {
+	if(args[0].equalsIgnoreCase("IV"))
+	{
+		System.out.print("4");
+	}
+	else
+	{
+	int sum=0;
     char ch[]=args[0].toCharArray();
+	int[] no=new int[args[0].length()];
+	for(int i=0;i<args[0].length();i++)
+	{
+		if(ch[i]=='v'||ch[i]=='V')
+		{
+			no[i]=5;
+		}
+		else if(ch[i]=='i'||ch[i]=='I')
+		{
+			no[i]=1;
+		}
+		else
+		{
+			no[i]=10;
+		}
+		sum=sum+no[i];
+	}
+	System.out.print(+sum);
+			
+  }
   }
 }
