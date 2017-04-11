7d587b6b3623cdf99ecea9ece36d62331ba02bc4
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-11 05:43:05 +0530

    Update swap_even_odd.java

--- swap_even_odd.java
+++ swap_even_odd.java
@@ -1,1 +1,13 @@
+class swap_even_odd
+{
+  public static void main(String args[])
+  {
+    char[] ch=args[0].toCharArray();
+    for(int i=0;i<args[0].length();i+2)
+    {
+      System.out.print(ch[i]+ch[i+1]);
+    }
+  }
+}
 
+      
