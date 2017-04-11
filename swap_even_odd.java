391d8c4a87b34c4bed39758986e882f6e6e5714b
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-11 05:50:06 +0530

    Update swap_even_odd.java

--- swap_even_odd.java
+++ swap_even_odd.java
@@ -3,10 +3,24 @@ class swap_even_odd
   public static void main(String args[])
   {
     char[] ch=args[0].toCharArray();
-    for(int i=0;i<args[0].length();i+2)
-    {
-      System.out.print(ch[i]+ch[i+1]);
+	if(args[0].length()%2==0)
+	{
+    for(int i=0;i<args[0].length();i=i+2)
+    {		
+	  System.out.print(ch[i+1]);
+      System.out.print(ch[i]);
     }
+	}
+	else
+	{
+    for(int i=0;i<args[0].length()-1;i=i+2)
+    {		
+	  System.out.print(ch[i+1]);
+      System.out.print(ch[i]);
+    }
+	System.out.print(ch[args[0].length()-1]);
+	}
+		
   }
 }
 
