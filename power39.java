54029242bf857171aab8eb4a3d9fbe46d06f674f
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-18 21:33:43 +0530

    Update power39.java

--- power39.java
+++ power39.java
@@ -1,1 +1,19 @@
-
+import java.math.*;
+class power39
+{
+  public static void main(String args[])
+  {
+    int n=Integer.valueOf(args[0]);
+    for(int i=0;i<=n/2;i++)
+    {
+      if(n==Math.pow(i,2))
+      {
+        System.out.print("POWER OF 2");
+      }
+      else
+      {
+        System.out.print("NOT POWER OF 2");
+      }
+    }
+  }
+}
