33c273c89d50befb197c293015c71e2ba5df18fb
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-18 21:07:53 +0530

    Update validate24.java

--- validate24.java
+++ validate24.java
@@ -1,1 +1,19 @@
-
+class validate24
+{
+  public static void main(String args[])
+  {
+    for(int i=0;i<args.length;i++)
+    {
+      int n1=args[i].length();
+      char[] c=args[i].toCharArray();
+      if(Character.isDigit(c[0])&&Character.isDigit(c[n1-1]))
+      {
+        System.out.println(args[i]+"=>TRUE");
+      }
+      else
+      {
+         System.out.println(args[i]+"=>FALSE");
+      }   
+    }
+  }
+}
