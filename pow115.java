f857df05ded745f8f9c6a7e280d8be56f242f11d
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-18 23:47:47 +0530

    Update pow115.java

--- pow115.java
+++ pow115.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+class pow115
+{
+  public static void main(String args[])
+  {
+    int sum=0;
+    int n=Integer.valueOf(args[0]);
+    while(n!=0)
+    {
+      int i=n%10;
+      sum=sum+pow(i,i);
+      n/10;
+    }
+    System.out.print(+sum);
+  }
+}
+  
