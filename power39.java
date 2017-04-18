f3dc34267b97bfc738a71ea35906a28ce16f4240
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-18 21:37:50 +0530

    Update power39.java

--- power39.java
+++ power39.java
@@ -3,17 +3,21 @@ class power39
 {
   public static void main(String args[])
   {
-    int n=Integer.valueOf(args[0]);
+    int n=Integer.valueOf(args[0]),flag=0;
     for(int i=0;i<=n/2;i++)
     {
       if(n==Math.pow(i,2))
       {
-        System.out.print("POWER OF 2");
+        System.out.print("POWER OF TWO");
+		flag=1;
+		break;
       }
-      else
+	}
+	
+      if(flag==0)
       {
-        System.out.print("NOT POWER OF 2");
+        System.out.print("NOT POWER OF TWO");
       }
-    }
+    
   }
 }
