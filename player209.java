09155d03e0dfc06234e1a6e514db98771ab0ac07
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-06 18:50:43 +0530

    Update player209.java

--- player209.java
+++ player209.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+class player209
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+    	System.out.println("ENTER THE STRING");
+   		String s=in.nextLine();
+   		String s1=in.nextLine();
+   		System.out.println(s+s1);
+        
+    }
+}
